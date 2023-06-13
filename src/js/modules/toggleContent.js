132849a76ebc911d1b08dfa229939817aa13ac80
Artem <mart_22@ukr.net>
2023-06-13 20:43:40 +0300

    Add toggleContent.js

---
+++ src/js/modules/toggleContent.js
@@ -0,0 +1,61 @@
+export default function toggleContent() {
+	document.addEventListener('click', toggleFunc());
+
+	function toggleFunc() {
+		return function (e) {
+			const target = e.target,
+				toggleWr = document.querySelector('.toggle-wrapper'),
+				toggleBtn = document.querySelector('.toggle-btn'),
+				toggleBtnEl = document.querySelectorAll('.toggle-btn span'),
+				toggleContent = document.querySelector('.toggle-wrapper > div'),
+				overlay = document.querySelector('.overlay');
+
+			for (let i = 0; i < toggleBtnEl.length; i++) {
+				if (target == toggleBtnEl[i]) {
+					toggleBtn.classList.toggle('toggle-btn-style');
+					toggleContent.classList.toggle('content-visible');
+					overlay.classList.toggle('overlay-visible');
+
+					if (document.querySelector('.toggle-btn-style')) {
+						document.body.style.paddingRight =
+							window.innerWidth - document.documentElement.clientWidth + 'px';
+						document.body.style.overflowY = 'hidden';
+					} else {
+						if (!document.querySelector('.modal-overlay.show')) {
+							document.body.style.overflowY = '';
+							document.body.style.paddingRight = '';
+						}
+					}
+				}
+			}
+
+			if (target == toggleWr || target == toggleBtn) {
+				toggleBtn.classList.toggle('toggle-btn-style');
+				toggleContent.classList.toggle('content-visible');
+				overlay.classList.toggle('overlay-visible');
+
+				if (document.querySelector('.toggle-btn-style')) {
+					document.body.style.paddingRight =
+						window.innerWidth - document.documentElement.clientWidth + 'px';
+					document.body.style.overflowY = 'hidden';
+				} else {
+					if (!document.querySelector('.modal-overlay.show')) {
+						document.body.style.overflowY = '';
+						document.body.style.paddingRight = '';
+					}
+				}
+			}
+
+			if (!target.closest('.toggle-wrapper') && !target.closest('.modal-btn')) {
+				toggleBtn.classList.remove('toggle-btn-style');
+				toggleContent.classList.remove('content-visible'),
+					overlay.classList.remove('overlay-visible');
+
+				if (!document.querySelector('.modal-overlay.show')) {
+					document.body.style.overflowY = '';
+					document.body.style.paddingRight = '';
+				}
+			}
+		};
+	}
+}
