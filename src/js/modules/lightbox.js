445ca4f1ab36b9965a71742fd5497c58298c91a5
Artem <mart_22@ukr.net>
2023-06-18 22:38:55 +0300

    Import, init sliders in lightbox.js

--- src/js/modules/lightbox.js
+++ src/js/modules/lightbox.js
@@ -1,6 +1,12 @@
+import sliders from './sliders';
+
 export default function lightbox({ container, selector, plugins, speed, zoom, download, mode }) {
 	if (document.querySelector(container)) {
 		if (document.querySelector('.desktop-user-agent')) {
+			document.querySelector(container).addEventListener('lgInit', (event) => {
+				sliders();
+			});
+
 			lightGallery(document.querySelector(container), {
 				// licenseKey: '0000-0000-000-0000',
 				selector,
@@ -12,5 +18,10 @@ export default function lightbox({ container, selector, plugins, speed, zoom, do
 				// counter: false,
 			});
 		}
+		if (document.querySelector('.mobile-user-agent')) {
+			sliders();
+		}
+	} else {
+		sliders();
 	}
 }
